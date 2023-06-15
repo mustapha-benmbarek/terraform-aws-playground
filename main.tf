34a88e447cde5bf0d30305124320036fd4883208
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-06-15 17:21:11 +0400

    Initial commit

--- main.tf
+++ main.tf
@@ -1,20 +1,28 @@
-/*Parameters..*/
+/*Parameters | Networking Stack..*/
 module "aws-networking" {
   source                                 = "github.com/mustapha-benmbarek/terraform-aws-core-networking"
-  file-vpcs                              = "${path.module}/parameters/vpcs.json"
-  file-vpc-subnets                       = "${path.module}/parameters/vpc-subnets.json"
-  file-vpc-internet-gateways             = "${path.module}/parameters/vpc-internet-gateways.json"
-  file-vpc-elastic-ips                   = "${path.module}/parameters/vpc-elastic-ips.json"
-  file-vpc-nat-gateways                  = "${path.module}/parameters/vpc-nat-gateways.json"
-  file-vpc-egress-only-internet-gateways = "${path.module}/parameters/vpc-egress-only-internet-gateways.json"
-  file-vpc-carrier-gateways              = "${path.module}/parameters/vpc-carrier-gateways.json"
-  file-vpc-dhcp-option-sets              = "${path.module}/parameters/vpc-dhcp-option-sets.json"
-  file-vpc-managed-prefix-lists          = "${path.module}/parameters/vpc-managed-prefix-lists.json"
-  file-vpc-endpoints                     = "${path.module}/parameters/vpc-endpoints.json"
-  file-vpc-endpoint-services             = "${path.module}/parameters/vpc-endpoint-services.json"
-  file-vpc-peerings                      = "${path.module}/parameters/vpc-peerings.json"
-  file-vpc-route-tables                  = "${path.module}/parameters/vpc-route-tables.json"
-  file-placement-groups                  = "${path.module}/parameters/placement-groups.json"
-  file-vpn-customer-gateways             = "${path.module}/parameters/vpn-customer-gateways.json"
-  file-vpn-virtual-private-gateways      = "${path.module}/parameters/vpn-virtual-private-gateways.json"
+  file-vpcs                              = "${local.path.networking}/vpcs.json"
+  file-vpc-subnets                       = "${local.path.networking}/vpc-subnets.json"
+  file-vpc-internet-gateways             = "${local.path.networking}/vpc-internet-gateways.json"
+  file-vpc-elastic-ips                   = "${local.path.networking}/vpc-elastic-ips.json"
+  file-vpc-nat-gateways                  = "${local.path.networking}/vpc-nat-gateways.json"
+  file-vpc-egress-only-internet-gateways = "${local.path.networking}/vpc-egress-only-internet-gateways.json"
+  //file-vpc-carrier-gateways              = "${local.path.networking}/vpc-carrier-gateways.json"
+  file-vpc-dhcp-option-sets     = "${local.path.networking}/vpc-dhcp-option-sets.json"
+  file-vpc-managed-prefix-lists = "${local.path.networking}/vpc-managed-prefix-lists.json"
+  //file-vpc-endpoints                     = "${local.path.networking}/vpc-endpoints.json"
+  //file-vpc-endpoint-services             = "${local.path.networking}/vpc-endpoint-services.json"
+  //file-vpc-peerings                      = "${local.path.networking}/vpc-peerings.json"
+  file-vpc-route-tables             = "${local.path.networking}/vpc-route-tables.json"
+  file-placement-groups             = "${local.path.networking}/placement-groups.json"
+  file-vpn-customer-gateways        = "${local.path.networking}/vpn-customer-gateways.json"
+  file-vpn-virtual-private-gateways = "${local.path.networking}/vpn-virtual-private-gateways.json"
+}
+
+/*Parameters | Security Stack..*/
+module "aws-security" {
+  source               = "github.com/mustapha-benmbarek/terraform-aws-core-security"
+  file-key-pairs       = "${local.path.security}/key-pairs.json"
+  file-network-acls    = "${local.path.security}/network-acls.json"
+  file-security-groups = "${local.path.security}/security-groups.json"
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+/*XXXXXXXXXXXXXXXXXX loading...*/
+locals {
+  path = {
+    networking = "${path.module}/parameters/networking/"
+    security   = "${path.module}/parameters/security/"
+    storage    = "${path.module}/parameters/storage/"
+    database   = "${path.module}/parameters/database/"
+  }
+}
