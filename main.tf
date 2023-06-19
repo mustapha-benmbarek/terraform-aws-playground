d0f68d4af114acbed2c7305e0175c360a492db49
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-06-19 15:13:28 +0400

    Initial commit

--- main.tf
+++ main.tf
@@ -7,16 +7,16 @@ module "aws-networking" {
   file-vpc-elastic-ips                   = "${local.path.networking}/vpc-elastic-ips.json"
   file-vpc-nat-gateways                  = "${local.path.networking}/vpc-nat-gateways.json"
   file-vpc-egress-only-internet-gateways = "${local.path.networking}/vpc-egress-only-internet-gateways.json"
-  //file-vpc-carrier-gateways              = "${local.path.networking}/vpc-carrier-gateways.json"
-  file-vpc-dhcp-option-sets     = "${local.path.networking}/vpc-dhcp-option-sets.json"
-  file-vpc-managed-prefix-lists = "${local.path.networking}/vpc-managed-prefix-lists.json"
-  //file-vpc-endpoints                     = "${local.path.networking}/vpc-endpoints.json"
-  //file-vpc-endpoint-services             = "${local.path.networking}/vpc-endpoint-services.json"
-  //file-vpc-peerings                      = "${local.path.networking}/vpc-peerings.json"
-  //file-vpc-route-tables             = "${local.path.networking}/vpc-route-tables.json"
-  file-placement-groups             = "${local.path.networking}/placement-groups.json"
-  file-vpn-customer-gateways        = "${local.path.networking}/vpn-customer-gateways.json"
-  file-vpn-virtual-private-gateways = "${local.path.networking}/vpn-virtual-private-gateways.json"
+  file-vpc-carrier-gateways              = "${local.path.networking}/vpc-carrier-gateways.json"
+  file-vpc-dhcp-option-sets              = "${local.path.networking}/vpc-dhcp-option-sets.json"
+  file-vpc-managed-prefix-lists          = "${local.path.networking}/vpc-managed-prefix-lists.json"
+  file-vpc-endpoints                     = "${local.path.networking}/vpc-endpoints.json"
+  file-vpc-endpoint-services             = "${local.path.networking}/vpc-endpoint-services.json"
+  file-vpc-peerings                      = "${local.path.networking}/vpc-peerings.json"
+  file-vpc-route-tables                  = "${local.path.networking}/vpc-route-tables.json"
+  file-placement-groups                  = "${local.path.networking}/placement-groups.json"
+  file-vpn-customer-gateways             = "${local.path.networking}/vpn-customer-gateways.json"
+  file-vpn-virtual-private-gateways      = "${local.path.networking}/vpn-virtual-private-gateways.json"
 }
 
 /*Security Stack..*/
@@ -28,22 +28,28 @@ module "aws-security" {
   file-network-acls = "${local.path.security}/network-acls.json"
 }
 
+
 /*Database Stack..*/
 module "aws-database" {
-  source                         = "github.com/mustapha-benmbarek/terraform-aws-core-database"
-  data-vpcs                      = module.aws-networking.vpcs
-  data-vpc-subnets               = module.aws-networking.vpc-subnets
-  file-rds-aurora-mysql-dbs      = "${local.path.security}/rds-aurora-mysql-dbs.json"
-  file-rds-aurora-postgresql-dbs = "${local.path.security}/rds-aurora-postgresql-dbs.json"
-  file-rds-mysql-dbs             = "${local.path.security}/rds-mysql-dbs.json"
-  file-rds-postgresql-dbs        = "${local.path.security}/rds-postgresql-dbs.json"
-  file-rds-mariadb-dbs           = "${local.path.security}/rds-mariadb-dbs.json"
-  file-rds-oracle-dbs            = "${local.path.security}/rds-oracle-dbs.json"
-  file-rds-mssql-dbs             = "${local.path.security}/rds-mssql-dbs.json"
-  file-documentdb-dbs            = "${local.path.security}/documentdb-dbs.json"
-  file-neptune-dbs               = "${local.path.security}/neptune-dbs.json"
-  file-elasticache-dbs           = "${local.path.security}/elasticache-dbs.json"
-  file-memorydb-redis-dbs        = "${local.path.security}/memorydb-redis-dbs.json"
-  file-quantum-ledger-dbs        = "${local.path.security}/quantum-ledger-dbs.json"
-  file-keyspaces-dbs             = "${local.path.security}/keyspaces-dbs.json"
+  source                           = "github.com/mustapha-benmbarek/terraform-aws-core-database"
+  data-vpcs                        = module.aws-networking.vpcs
+  data-vpc-subnets                 = module.aws-networking.vpc-subnets
+  file-rds-aurora-mysql-dbs        = "${local.path.database}/rds-aurora-mysql-dbs.json"
+  file-rds-aurora-postgresql-dbs   = "${local.path.database}/rds-aurora-postgresql-dbs.json"
+  file-rds-mysql-dbs               = "${local.path.database}/rds-mysql-dbs.json"
+  file-rds-postgresql-dbs          = "${local.path.database}/rds-postgresql-dbs.json"
+  file-rds-mariadb-dbs             = "${local.path.database}/rds-mariadb-dbs.json"
+  file-rds-oracle-dbs              = "${local.path.database}/rds-oracle-dbs.json"
+  file-rds-mssql-dbs               = "${local.path.database}/rds-mssql-dbs.json"
+  file-documentdb-dbs              = "${local.path.database}/documentdb-dbs.json"
+  file-documentdb-subnet-groups    = "${local.path.database}/documentdb-subnet-groups.json"
+  file-documentdb-parameter-groups = "${local.path.database}/documentdb-parameter-groups.json"
+  file-neptune-dbs                 = "${local.path.database}/neptune-dbs.json"
+  file-neptune-subnet-groups       = "${local.path.database}/neptune-subnet-groups.json"
+  file-neptune-parameter-groups    = "${local.path.database}/neptune-parameter-groups.json"
+  file-elasticache-dbs             = "${local.path.database}/elasticache-dbs.json"
+  file-memorydb-redis-dbs          = "${local.path.database}/memorydb-redis-dbs.json"
+  file-quantum-ledger-dbs          = "${local.path.database}/quantum-ledger-dbs.json"
+  file-keyspaces-dbs               = "${local.path.database}/keyspaces-dbs.json"
+  file-timestream-dbs              = "${local.path.database}/timestream-dbs.json"
 }
