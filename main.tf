40f6b978314fa5b6d3c4bf5df28d12aefd9bd806
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-09-26 21:02:53 +0400

    Initial commit

--- main.tf
+++ main.tf
@@ -7,19 +7,19 @@ module "aws-networking" {
   file-vpc-elastic-ips                   = "${local.path.networking}/vpc-elastic-ips.json"
   file-vpc-nat-gateways                  = "${local.path.networking}/vpc-nat-gateways.json"
   file-vpc-egress-only-internet-gateways = "${local.path.networking}/vpc-egress-only-internet-gateways.json"
-  file-vpc-carrier-gateways              = "${local.path.networking}/vpc-carrier-gateways.json"
-  file-vpc-dhcp-option-sets              = "${local.path.networking}/vpc-dhcp-option-sets.json"
-  file-vpc-managed-prefix-lists          = "${local.path.networking}/vpc-managed-prefix-lists.json"
-  file-vpc-endpoints                     = "${local.path.networking}/vpc-endpoints.json"
-  file-vpc-endpoint-services             = "${local.path.networking}/vpc-endpoint-services.json"
-  file-vpc-peerings                      = "${local.path.networking}/vpc-peerings.json"
-  file-vpc-route-tables                  = "${local.path.networking}/vpc-route-tables.json"
-  file-placement-groups                  = "${local.path.networking}/placement-groups.json"
-  file-vpn-customer-gateways             = "${local.path.networking}/vpn-customer-gateways.json"
-  file-vpn-virtual-private-gateways      = "${local.path.networking}/vpn-virtual-private-gateways.json"
+  //file-vpc-carrier-gateways              = "${local.path.networking}/vpc-carrier-gateways.json"
+  file-vpc-dhcp-option-sets     = "${local.path.networking}/vpc-dhcp-option-sets.json"
+  file-vpc-managed-prefix-lists = "${local.path.networking}/vpc-managed-prefix-lists.json"
+  //file-vpc-endpoints                     = "${local.path.networking}/vpc-endpoints.json"
+  //file-vpc-endpoint-services             = "${local.path.networking}/vpc-endpoint-services.json"
+  file-vpc-peerings                 = "${local.path.networking}/vpc-peerings.json"
+  file-vpc-route-tables             = "${local.path.networking}/vpc-route-tables.json"
+  file-placement-groups             = "${local.path.networking}/placement-groups.json"
+  file-vpn-customer-gateways        = "${local.path.networking}/vpn-customer-gateways.json"
+  file-vpn-virtual-private-gateways = "${local.path.networking}/vpn-virtual-private-gateways.json"
 }
 
-/*Security Stack..
+/*Security Stack..*/
 module "aws-security" {
   source                       = "github.com/mustapha-benmbarek/terraform-aws-core-security"
   data-vpcs                    = module.aws-networking.vpcs
@@ -29,7 +29,7 @@ module "aws-security" {
   file-acm-public-certificates = "${local.path.security}/acm-public-certificates.json"
 }
 
-/*Database Stack..
+/*Database Stack..*/
 module "aws-database" {
   source                           = "github.com/mustapha-benmbarek/terraform-aws-core-database"
   data-vpcs                        = module.aws-networking.vpcs
@@ -53,4 +53,3 @@ module "aws-database" {
   file-keyspaces-dbs               = "${local.path.database}/keyspaces-dbs.json"
   file-timestream-dbs              = "${local.path.database}/timestream-dbs.json"
 }
-*/
