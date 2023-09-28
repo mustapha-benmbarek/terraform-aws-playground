8c2733f726411f505194025e5ffaafe06bb2bcd5
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2023-09-28 16:17:58 +0400

    Initial commit

--- main.tf
+++ main.tf
@@ -1,35 +1,42 @@
 /*Networking Stack..*/
 module "aws-networking" {
-  source                                 = "github.com/mustapha-benmbarek/terraform-aws-core-networking"
-  file-vpcs                              = "${local.path.networking}/vpcs.json"
-  file-vpc-subnets                       = "${local.path.networking}/vpc-subnets.json"
-  file-vpc-internet-gateways             = "${local.path.networking}/vpc-internet-gateways.json"
+  source    = "../terraform-aws-core-networking"
+  file-vpcs = "${local.path.networking}/vpcs.json"
+  //file-vpc-subnets = "${local.path.networking}/vpc-subnets.json"
+  /*file-vpc-internet-gateways = "${local.path.networking}/vpc-internet-gateways.json"
   file-vpc-elastic-ips                   = "${local.path.networking}/vpc-elastic-ips.json"
   file-vpc-nat-gateways                  = "${local.path.networking}/vpc-nat-gateways.json"
   file-vpc-egress-only-internet-gateways = "${local.path.networking}/vpc-egress-only-internet-gateways.json"
-  //file-vpc-carrier-gateways              = "${local.path.networking}/vpc-carrier-gateways.json"
-  file-vpc-dhcp-option-sets     = "${local.path.networking}/vpc-dhcp-option-sets.json"
-  file-vpc-managed-prefix-lists = "${local.path.networking}/vpc-managed-prefix-lists.json"
-  //file-vpc-endpoints                     = "${local.path.networking}/vpc-endpoints.json"
-  //file-vpc-endpoint-services             = "${local.path.networking}/vpc-endpoint-services.json"
-  file-vpc-peerings                 = "${local.path.networking}/vpc-peerings.json"
-  file-vpc-route-tables             = "${local.path.networking}/vpc-route-tables.json"
-  file-placement-groups             = "${local.path.networking}/placement-groups.json"
-  file-vpn-customer-gateways        = "${local.path.networking}/vpn-customer-gateways.json"
-  file-vpn-virtual-private-gateways = "${local.path.networking}/vpn-virtual-private-gateways.json"
+  file-vpc-carrier-gateways              = "${local.path.networking}/vpc-carrier-gateways.json"
+  file-vpc-dhcp-option-sets              = "${local.path.networking}/vpc-dhcp-option-sets.json"
+  file-vpc-managed-prefix-lists          = "${local.path.networking}/vpc-managed-prefix-lists.json"
+  file-vpc-endpoints                     = "${local.path.networking}/vpc-endpoints.json"
+  file-vpc-endpoint-services             = "${local.path.networking}/vpc-endpoint-services.json"
+  file-vpc-peerings                      = "${local.path.networking}/vpc-peerings.json"
+  file-vpc-route-tables                  = "${local.path.networking}/vpc-route-tables.json"
+  file-placement-groups      = "${local.path.networking}/placement-groups.json"
+  file-vpn-customer-gateways = "${local.path.networking}/vpn-customer-gateways.json"
+  file-vpn-virtual-private-gateways = "${local.path.networking}/vpn-virtual-private-gateways.json"*/
+
+  file-r53-public-hosted-zones  = "${local.path.networking}/r53-public-hosted-zones.json"
+  file-r53-private-hosted-zones = "${local.path.networking}/r53-private-hosted-zones.json"
 }
 
-/*Security Stack..*/
+
+/*Security Stack..
 module "aws-security" {
-  source                       = "github.com/mustapha-benmbarek/terraform-aws-core-security"
-  data-vpcs                    = module.aws-networking.vpcs
-  data-vpc-subnets             = module.aws-networking.vpc-subnets
-  file-key-pairs               = "${local.path.security}/key-pairs.json"
-  file-network-acls            = "${local.path.security}/network-acls.json"
-  file-acm-public-certificates = "${local.path.security}/acm-public-certificates.json"
-}
+  source = "../terraform-aws-core-security"
+  //data-vpcs                    = module.aws-networking.vpcs
+  //data-vpc-subnets             = module.aws-networking.vpc-subnets
+  //file-key-pairs               = "${local.path.security}/key-pairs.json"
+  //file-network-acls            = "${local.path.security}/network-acls.json"
+  //file-acm-public-certificates = "${local.path.security}/acm-public-certificates.json"
+  //file-r53-dns-firewall-domain-lists = "${local.path.security}/r53-dns-firewall-domain-lists.json"
+  //file-r53-dns-firewall-rules        = "${local.path.security}/r53-dns-firewall-rules.json"
+  //file-r53-dns-firewall-rule-groups  = "${local.path.security}/r53-dns-firewall-rule-groups.json"
+}*/
 
-/*Database Stack..*/
+/*Database Stack..
 module "aws-database" {
   source                           = "github.com/mustapha-benmbarek/terraform-aws-core-database"
   data-vpcs                        = module.aws-networking.vpcs
@@ -53,3 +60,4 @@ module "aws-database" {
   file-keyspaces-dbs               = "${local.path.database}/keyspaces-dbs.json"
   file-timestream-dbs              = "${local.path.database}/timestream-dbs.json"
 }
+*/
